2cda37197df91f5752e9d3e86c07c7217706200a
Sonia Verma <soniaverma@Sonias-MacBook-Pro.local>
2025-10-29 22:30:40 +0530

    updating App

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
 import './App.css'
 import JsonForm from './components/JsonForm'
 import JsonTree from './components/JsonTree'
-
-const nodes= [
-  {
-      id : "1",
-      data : {
-          label : "Node 1"
-      },
-      position :{ x: 200, y: 200}
-
-  },
-  {
-      id : "2",
-      data : {
-          label : "Node 1"
-      },
-      position :{ x: 300, y: 300}
-
-  }
-];
+import ErrorDisplay from './components/ErrorDisplay'
+import React from 'react'
+import useJsonParser from './hooks/useJsonParser'
 
 function App() {
+  const {jsonInput,setJsonInput, nodes, edges, error, parseAndConvert} = useJsonParser(); 
 
   return (
     <>
     <div className="mainDiv">
-    <JsonForm />
-    <JsonTree incomingNodes={nodes}/>
+    <JsonForm jsonInput={jsonInput} setJsonInput={setJsonInput} convertJsonAsNodesAndEdges={parseAndConvert}/>
+    {error && <ErrorDisplay message={error} />}
+    <JsonTree nodes={nodes} edges={edges}/>
     </div>
     </>
   )
