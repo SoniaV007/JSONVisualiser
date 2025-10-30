3dd503b603b51cd5f93f05fbb170923de8680d79
Sonia Verma <soniaverma@Sonias-MacBook-Pro.local>
2025-10-30 19:09:22 +0530

    using reactflow hook

--- src/components/JsonTree.jsx
+++ src/components/JsonTree.jsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import "../App.css"
 import 'reactflow/dist/style.css';
 
 import ReactFlow, {
-    MiniMap,
     Controls,
     Background,
-    Position,
+    ReactFlowProvider,
+    useReactFlow,
   } from 'reactflow';
 
-const JsonTree = ({nodes, edges}) => {
+const JsonTreeContent = ({nodes, edges, matchedNode}) => {
+  const reactFlowInstance = useReactFlow();
+  const prevMatchedNode = useRef(null);
+
+  useEffect(() => {
+    if(matchedNode && matchedNode !== prevMatchedNode.current) {
+      const node = nodes.find(n => n.id === matchedNode.nodeId);
+      if(node && reactFlowInstance) {
+        reactFlowInstance.setCenter(
+          node.position.x + 100,
+          node.position.y + 40,
+          { zoom: 1, duration: 800 }
+        );
+      }
+      prevMatchedNode.current = matchedNode;
+    }
+  }, [matchedNode, nodes, reactFlowInstance]);
 
   return (
+    <ReactFlow nodes={nodes} edges={edges} fitView>
+      <Background />
+      <Controls />
+    </ReactFlow>
+  );
+}
+
+const JsonTree = ({nodes, edges, matchedNode}) => {
+  return (
     <div className='jsonTreeMainDiv'>
-        <ReactFlow nodes={nodes} edges={edges} fitView>
-          <Background />
-          <Controls />
-        </ReactFlow>
+      <ReactFlowProvider>
+        <JsonTreeContent nodes={nodes} edges={edges} matchedNode={matchedNode} />
+      </ReactFlowProvider>
     </div>
   )
 }
